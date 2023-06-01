e5e2bd06bc55cb6c0af7bc12ca923735b386e7c0
IbrahimSabriOrene <120184648+IbrahimSabriOrene@users.noreply.github.com>
2023-06-01 08:24:32 +0300

    Add project files.

---
+++ DBContext/Models/Categories.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBContext.Models;
+public class Categories
+{
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    public int Id { get; set; }
+    public string Dishes { get; set; }
+    public string Desserts { get; set; }
+    public string Soups { get; set; }
+
+}
---
+++ DBContext/Models/Recipe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBContext.Models;
+public class Recipe
+{
+    public string Title { get; set; }
+    public List<string> Ingredients { get; set; }
+    public string Instructions { get; set; }
+    public List<Categories> Category { get; set; }
+}
